60668ed21581ac66f8b27b32f7f9900fedc96bca
shuvomohajan <shuvomohajan@gmail.com>
2024-05-08 16:45:40 +0600

    chore: report opt-in view change & condition applied

--- src/views/reportOptIn.php
+++ src/views/reportOptIn.php
@@ -1,22 +1,23 @@
 <div class="updated" style="border: 1px solid #c3c4c7; padding: 15px">
 	<h1 style="padding: 0;">We hope you love <?php echo esc_attr($args['title']); ?>.</h1>
 
-	<p style="margin-bottom: 10px;">
+	<p style="margin-bottom: 15px;">
 		<?php echo esc_attr($args['description']); ?>
 	</p>
 
-	<div style="margin-bottom: 15px;">
-		<?php if ($args['termsUrl'] !== '') { ?>
-		<a href="<?php echo esc_attr($args['termsUrl']); ?>" style="text-decoration: none;">Terms of service</a> &nbsp; &nbsp;
-		<?php } ?>
+	<div style="display: flex; justify-content: space-between;">
+		<div>
+			<a href="<?php echo esc_attr(esc_url($args['optInUrl'])); ?>" class="button-primary button-large">Allow & Continue</a> &nbsp;
+			<a href="<?php echo esc_attr(esc_url($args['optOutUrl'])); ?>" class="button-secondary button-large" style="border-color: transparent">Skip</a>
+		</div>
+		<div>
+			<?php if ($args['termsUrl'] !== '') { ?>
+			<a href="<?php echo esc_attr($args['termsUrl']); ?>">Terms of service</a> &nbsp; &nbsp;
+			<?php } ?>
 
-		<?php if ($args['policyUrl'] !== '') { ?>
-			<a href="<?php echo esc_attr($args['policyUrl']); ?>" style="text-decoration: none;">Privacy Policy</a>
-		<?php } ?>
-	</div>
-
-	<div>
-		<a href="<?php echo esc_attr(esc_url($args['optInUrl'])); ?>" class="button-primary button-large">Allow & Continue</a> &nbsp;
-		<a href="<?php echo esc_attr(esc_url($args['optOutUrl'])); ?>" class="button-secondary button-large" style="border-color: transparent">Skip</a>
+			<?php if ($args['policyUrl'] !== '') { ?>
+				<a href="<?php echo esc_attr($args['policyUrl']); ?>">Privacy Policy</a>
+			<?php } ?>
+		</div>
 	</div>
 </div>
